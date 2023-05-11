47f6d5ab5b772c4d10ca8932c4bac7f1547e1774
Biel Altimira <biel.altimira@estudiantat.upc.edu>
2023-05-11 21:07:02 +0200

    billboard module documented and linted PEP8 stds

--- billboard.py
+++ billboard.py
@@ -2,12 +2,14 @@ from dataclasses import dataclass
 import requests
 from requests.exceptions import HTTPError
 from bs4 import BeautifulSoup, Tag
-from typing import Iterator 
+from typing import Iterator
 import json
+# types-beautifulsoup4
 
 
-@dataclass 
-class Film: 
+@dataclass
+class Film:
+    """Dataclass that stores information about a film."""
     title: str
     genre: str
     director: str
@@ -15,15 +17,17 @@ class Film:
     ...
 
 
-@dataclass 
-class Cinema: 
+@dataclass
+class Cinema:
+    """Dataclass that stores information about a cinema."""
     name: str
     address: str
     ...
 
 
-@dataclass 
-class Projection: 
+@dataclass
+class Projection:
+    """Dataclass that stores information about a film being projected."""
     film: Film
     cinema: Cinema
     time: tuple[int, int]   # hora:minut
@@ -31,75 +35,96 @@ class Projection:
     ...
 
 
-@dataclass 
-class Billboard: 
+@dataclass
+class Billboard:
+    """Dataclass that holds all the data of the current films on display."""
     films: list[Film]
     cinemas: list[Cinema]
     projections: list[Projection]
 
 
-def get_addr(soup) -> Iterator[str]:
-    for addr in soup.find_all("span", attrs={"class":"lighten"}):
+def get_addr(soup: BeautifulSoup) -> Iterator[str]:
+    """Generator that yields as strings the addreces of all the cinemas given the soup object."""
+    for addr in soup.find_all("span", attrs={"class": "lighten"}):
         if len(addr["class"]) == 1:
             yield (addr.text).strip()
 
 
-def get_cinema(soup) -> Iterator[str]:
-    for cinema in soup.find_all("h2", attrs={"class":"tt_18"}):
+def get_cinema(soup: BeautifulSoup) -> Iterator[str]:
+    """
+    Generator that yields strings corresponding to the names of the cinemas found on the soup object.
+    """
+    for cinema in soup.find_all("h2", attrs={"class": "tt_18"}):
         yield (cinema.text).strip()
 
 
-def get_projections(soup) -> Iterator[Tag]:
-    for table in soup.find_all("div", attrs={"class":"tabs_box_panels"}):
-        yield table.find("div", attrs={"class":"tabs_box_pan item-0"})
+def get_projections(soup: BeautifulSoup) -> Iterator[Tag]:
+    """
+    Generator that yields a parsed HTML (tag) of the different blocks of projections that each cinema offers.
+    """
+    for table in soup.find_all("div", attrs={"class": "tabs_box_panels"}):
+        yield table.find("div", attrs={"class": "tabs_box_pan item-0"})
 
 
-def get_film_data(soup) -> tuple[str, str, str, list[str]]:
-    dataJSON = soup.find("div", attrs={"class": "j_w"} )
-    data= json.loads(dataJSON.attrs["data-movie"])
-    return data["title"], data["genre"][0], data["directors"][0], data["actors"]
+def get_film_data(soup: Tag) -> tuple[str, str, str, list[str], str]:
+    """
+    Given the parsed HTML tag that hold film data, it scrapes all the required attributes for the film dataclass and returns them as a tuple.
+    """
+    tags = [tag.text.strip()
+            for tag in soup.find_all("span", attrs={"class": "bold"})]
+    lang = "Dubbed" if "Versión Doblada" in tags else "Original"
+
+    dataJSON = soup.find("div", attrs={"data-movie": True})
+    # it makes sure we obtained the right data type for mypy purposes
+    if isinstance(dataJSON, Tag):
+        data = json.loads(dataJSON.attrs["data-movie"])
+        return data["title"], data["genre"][0], data["directors"][0], data["actors"], lang
+    else:
+        raise AttributeError(
+            "Accessing attributes of something is not an HTML tag.")
 
 
 def read() -> Billboard:
+    """
+    It scrapes the Sensacine webpage to return a Billboard object with all the film-related data.
+    """
     url = "https://www.sensacine.com/cines/cines-en-72480/"
+    billboard = Billboard(list(), list(), list())
+
+    # It iterates over the 3 sub-pages
     for i in range(1, 4):
         try:
             page = requests.get(f"{url}?page={i}")
             page.raise_for_status()
-        
+
         except HTTPError as http_err:
-            print(f'HTTP error occurred: {http_err}')  
+            print(f'HTTP error occurred: {http_err}')
         except Exception as err:
-            print(f'Other error occurred: {err}')  
+            print(f'Other error occurred: {err}')
         else:
             print('----Success!-----')
-        
+
         soup = BeautifulSoup(page.text, "html.parser")
 
-        cinema_list:  list[Cinema] = list()
-        projection_list: list[Projection] = list()
-        film_list: list[Film] = list()
-
-        registered: list[str] = list()
-
+        # It uses generators to get the data ordered by cinema.
         for cinema_name, addr, projections_block in zip(get_cinema(soup), get_addr(soup), get_projections(soup)):
             cinema = Cinema(cinema_name, addr)
-            cinema_list.append(cinema)
+            billboard.cinemas.append(cinema)
 
-            for film_data in projections_block.find_all("div", attrs={"class":"item_resa"}):
-                title, genre, director, actors = get_film_data(film_data)
+            # It gathers the data of each film on the projections block
+            for film_data in projections_block.find_all("div", attrs={"class": "item_resa"}):
+                title, genre, director, actors, lang = get_film_data(film_data)
                 film = Film(title, genre, director, actors)
-                
-                if title not in registered:
-                    registered.append(title)
-                    film_list.append(film)
 
-                # sessions
+                # If the film is not registered yet, it gets added
+                if not any(f.title == title for f in billboard.films):
+                    billboard.films.append(film)
+
+                # It creates an entry for each projection of the film
                 for projection in film_data.find_all("em"):
-                    time = tuple(projection.text.strip().split(":"))
+                    time_parts = projection.text.strip().split(":")
+                    time = (int(time_parts[0]), int(time_parts[1]))
+                    billboard.projections.append(
+                        Projection(film, cinema, time, lang))
 
-                    projection_list.append(Projection(film, cinema, time, "ESPAÑOL"))
-    return Billboard(film_list, cinema_list, projection_list)
-     
-
-print(read())
+    return billboard
